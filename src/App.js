e71bc5a8668c44ddb537d8c0d0030597b72c7f66
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-06 17:40:15 +0000

    creating a page for a user socialmedia profile page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+    state = {
+      firstname:null,
+      lastname:null, 
+      email:null
+    }
+    
+    handleCLick = () => {
+        console.log(this.state.firstname);
+    }
+    
+    setFirstName = (e) => {
+      this.setState({
+        firstname: e.target.value    
+      });
+    }
+    
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>My App</p>
+        <input onChange={ this.setFirstName } placeholder="First Name" />
+        <button onClick={ this.handleCLick }>Submit</button>
+        
       </div>
     );
   }
