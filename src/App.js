23d070393559eeb11aeab9ce3099eef1fc9bb262
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-04-10 22:23:12 +0000

    completed files from class walkthrough

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Input from "./component/Input";
+import Display from "./component/Display";
 
 class App extends Component {
-    state = {
-      firstname:null,
-      lastname:null, 
-      email:null
-    }
-    
-    handleCLick = () => {
-        console.log(this.state.firstname);
-         axios.get('https://api.lyrics.ovh/v1/coldplay/paradise')
-        .then(res => {
-            // need to grab the data we retrieved and save it to our template
-            console.log(res.data);
-            // this.setState({
-            //     posts: res.data.slice(0, 10)
-            // });
-        });
-    }
-    
-    setFirstName = (e) => {
-      this.setState({
-        firstname: e.target.value    
-      });
-    }
-    
+  state={
+    artistname: null,
+    songname: null,
+    lyrics: null
+  }
+  
+  setArtist = name => {
+    this.setState({
+      artistname: name
+    })
+  }
+  setSong = name => {
+    this.setState({
+      songname: name
+    })
+  }
+  setLyrics = lyrics =>{
+    this.setState({lyrics})
+  }
+  
   render() {
     return (
-      <div className="App">
-        <p>My App</p>
-        <input onChange={ this.setFirstName } placeholder="First Name" />
-        <button onClick={ this.handleCLick }>Submit</button>
+      <div className="App" 
+        style={{
+          "textAlign":"center", 
+          "margin": "auto",
+          "width":"90%"
+        }}
+      >
+        <h2>Search for a song </h2>
+        <Input 
+          setArtistName = {this.setArtist}
+          setSongName = {this.setSong}
+          setLyricsText = {this.setLyrics}
+        />
         
+        <Display 
+          artist={this.state.artistname} 
+          song= {this.state.songname}
+          lyrics={this.state.lyrics} 
+        />
       </div>
     );
   }
